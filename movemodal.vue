b60d09b693475e53d67a17c221c813875f3eccf6
chenlifei <feilichen@yeah.net>
2021-11-17 18:45:16 +0800

    fix

--- movemodal.vue
+++ movemodal.vue
@@ -100,6 +100,224 @@ export default {
 }
 </script>
 
+.drag-image {
+    position: absolute;
+    width: 150px;
+    height: 100px;
+    object-fit: cover;
+    z-index: 10;
+}
+/deep/ .multiple_drag_start {
+    &::before {
+        content: '';
+        z-index: 0;
+        position: absolute;
+        top: 10px;
+        left: 10px;
+        width: 150px;
+        height: 100px;
+        border: 1px dashed var(--mainColor);
+    }
+}
+/deep/ .drag {
+    .project-grid-container {
+        &::before {
+            content: '';
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            border-radius: 5px;
+            background-color: #E3E6EC;
+            border: 1px dashed #D6D8DD;
+            z-index: 10;
+        }
+    }
+}
+/deep/ .enter-folder {
+    padding: 10px 0;
+    border: 1px dashed #0EC5C7;
+    background-color: rgba(14, 197, 199, 0.1);
+    img {
+        display: block;
+        width: 90% !important;
+        height: 159px !important;
+        margin: auto;
+    }
+    .folder-bottom {
+        width: 90% !important;
+        height: 78px !important;
+        margin: auto;
+    }
+}
+
+// let dragged = null;
+        // let folderNode;
+
+        /* 拖动目标元素时触发drag事件 */
+        /* document.addEventListener('drag', (event) => {
+        }, false); */
+
+        /* document.addEventListener('dragstart', (event) => {
+            // event.preventDefault();
+            console.log(event);
+            console.log('start');
+            dragged = event.target;
+            $(dragged).css({
+                position: 'relative',
+                left: event.offsetX - 30,
+                top: event.offsetY - 50
+            });
+            event.target.classList.add('drag_start');
+            if (this.isBatching) {
+                event.target.classList.add('multiple_drag_start');
+                setTimeout(() => {
+                    this.selectListId.forEach(item => {
+                        $(`.project-card[data-id=${item}] .project-grid-container`).addClass('drag');
+                    });
+                }, 0);
+                
+            }
+            setTimeout(() => {
+                event.target.classList.add('drag');
+            }, 0)
+        }, false);
+
+        document.addEventListener('dragend', (event) => {
+            console.log('end');
+            setTimeout(() => {
+                event.target.classList.remove('drag_start');
+                event.target.classList.remove('drag');
+                $(dragged).css({
+                    position: 'relative',
+                    left: 0,
+                    top: 0
+                });
+                if (this.isBatching) {
+                    event.target.classList.remove('multiple_drag_start');
+                    this.selectListId.forEach(item => {
+                        $(`.project-card[data-id=${item}] .project-grid-container`).removeClass('drag');
+                    });
+                }
+                if(folderNode && folderNode.length) folderNode.get(0).classList.remove('enter-folder');
+            }, 20);
+        }, false);
+
+        document.addEventListener('dragover', (event) => {
+            // 阻止默认动作以启用drop
+            event.preventDefault();
+            dragged.classList.add('drag');
+        }, false);
+
+        document.addEventListener('dragenter', (event) => {
+            let parent = $(event.target).parents('.folder');
+            // 可拖动的元素进入了可放置的目标
+            if (parent.length) {
+                parent.get(0).classList.add('enter-folder');
+            }
+        }, false);
+
+        document.addEventListener('dragleave', (event) => {
+            // 拖动元素离开可放置目标节点，清除样式
+            let parent = $(event.target).parents('.folder');
+            if (parent.length) {
+                parent.get(0).classList.remove('enter-folder');
+            }
+        }, false);
+
+        document.addEventListener('drop', (event) => {
+            console.log('drop');
+            event.preventDefault();
+            // 将拖动的元素放到所选择的可放置目标节点中
+            folderNode = $(event.target).parents('.folder');
+            
+            if (folderNode.length) {
+                let data;
+                let targetCardNode = folderNode.parents('.project-card').get(0);
+                let dragCardNode = $(dragged).parents('.project-card').get(0);
+
+                data = {
+                    targetId: targetCardNode.getAttribute('data-id'),
+                    dragId: dragCardNode.getAttribute('data-id'),
+                    isFolder: dragCardNode.getAttribute('data-folder')
+                };
+                this.move(data);
+            }
+        }, false); */
+    
+        let dragging;
+        let draggingEle;
+        let dragId, isFolder;
+        document.addEventListener('mousedown', (event) => {
+            let parent = $(event.target).parents('.project-card');
+            
+            if (parent.length) {
+                dragging = event.target;
+                dragId = parent.attr('data-id');
+                isFolder = parent.attr('data-folder');
+                console.log(dragId, isFolder);
+                setTimeout(() => {
+                    console.log(dragging);
+                }, 200);
+                return;
+                let img;
+                if (parent.get(0).hasAttribute('data-folder')) {
+                    img = parent.find('.folder img').clone();
+                } else {
+                    img = parent.find('.cover-wrapper img').clone();
+                }
+                parent.addClass('drag');
+                draggingEle = img.get(0);
+                
+                document.body.appendChild(draggingEle);
+                draggingEle.style.position = 'absolute';
+                draggingEle.style.width = '150px';
+                draggingEle.style.height = '100px';
+                draggingEle.style.objectFit = 'cover';
+                draggingEle.style.zIndex = '99';
+                draggingEle.style.borderRadius = '5px';
+                draggingEle.style.border = '1px dashed #0fcdcf';
+                
+                if (this.isBatching) {
+                    event.target.classList.add('multiple_drag_start');
+                    this.selectListId.forEach(item => {
+                        $(`.project-card[data-id=${item}] .project-grid-container`).addClass('drag');
+                    });
+                }
+            }
+        }, false);
+
+        document.addEventListener('mouseup', (event) => {
+            dragging = null;
+            if(draggingEle) {
+                document.body.removeChild(draggingEle);
+                draggingEle = null;
+                $(`.project-card[data-id=${dragId}]`).removeClass('drag');
+            };
+            setTimeout(() => {
+                if (this.isBatching) {
+                    event.target.classList.remove('multiple_drag_start');
+                    this.selectListId.forEach(item => {
+                        $(`.project-card[data-id=${item}] .project-grid-container`).removeClass('drag');
+                    });
+                }
+                // if (folderNode && folderNode.length) folderNode.get(0).classList.remove('enter-folder');
+            }, 20);
+        }, false);
+
+        document.addEventListener('mousemove', (event) => {
+            event.preventDefault();
+            if (dragging && draggingEle) {
+                draggingEle.style.left = event.x + 'px';
+                draggingEle.style.top = event.y + 'px';
+            }
+        }, false);
+
+        document.addEventListener('mouseenter', (event) => {
+            event.preventDefault();
+        }, false);
+
 <style lang="less" scoped>
 .move-modal {
     position: relative;
