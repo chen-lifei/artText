804670d8184340cddd06743fbdf3f480aaf8ca9b
chenlifei <feilichen@yeah.net>
2022-01-18 16:57:31 +0800

    update

--- woodo/src/assets/pc/js/svg.transform.js
+++
@@ -1,8 +0,0 @@
-let $ = require('./jquery.min.js');
-
-$(function(){
-	
-	 jQuery.fn.alert=function(){
-            alert("我的自定义jquery方法1");
-     }
-})
--- woodo/src/store/getters.js
+++
@@ -1,5 +0,0 @@
-const getters = {
-	resData: state => state.resData,
-    pageTheme: state => state.theme.pageTheme,
-}
-export default getters
